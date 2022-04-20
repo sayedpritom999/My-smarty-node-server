b6914d18ed04feeaf28f5754082302c4ef1f5152
unknown <sayedpritom999@gmail.com>
2022-04-21 02:22:23 +0600

    steps files added

---
+++ _steps.js
@@ -0,0 +1,21 @@
+/**
+ * 1. Create a folder (manually or mkdir)
+ * 2. open command line to that folder 
+ * 3. npm init 
+ * 4. npm install express
+ * 5. create index.js file
+ * ---------------------------------------
+ * For express/Node server
+ * ---------------------------------------
+ * 1. require('express')
+ * 2. create app variable
+ * 3. declare port 
+ * 4. set app.get('/') 
+ * 5. listen to port 
+ * 6. run node index.js 
+ * 7. check in browser for that port 
+ * 
+ * 
+ * 
+ * 
+ */
